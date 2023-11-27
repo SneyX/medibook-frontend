32543a63dd80e4aa223df7c7781538de6bc64e7a
Victor Gutierrez <victor.gutierrez@wundermanthompson.com>
2023-11-27 13:15:26 -0300

    cards #17 & #18

--- src/components/DataDialog.vue
+++ src/components/DataDialog.vue
@@ -6,12 +6,35 @@
         <div class="categoryCont">
           <p><span>ID:</span> {{ dialog.id }}</p>
           <p><span>NOMBRE:</span> {{ dialog.name }}</p>
-          <p><span>DESCRIPCIÓN:</span> {{ dialog.description }}</p>
+          <p v-if="dialog.description"><span>DESCRIPCIÓN:</span> {{ dialog?.description }}</p>
         </div>
       </div>
       <div class="btnCont">
         <div class="Btn" @click="dialog.acept">ACEPTAR</div>
         <div class="Btn" @click="dialog.cancel">CANCELAR</div>
+      </div>
+    </div>
+  </div>
+  <div v-if="dialog.type == 'modificarCarac'" class="caracContenedor">
+    <div class="modificarCarac">
+      <label>CARACTERÍSTICAS*</label>
+      <div class="caractCont">
+        <div class="carac" v-for="caracteristica in dialog.totalCarac" :key="caracteristica.id" @click=agregar(caracteristica)>
+          <DinamicIcon :iconName="caracteristica?.urlicon" class="icono"/>
+          <p>{{ caracteristica.name }}</p>
+        </div>
+      </div>
+      <label>AGREGADAS*</label>
+      <div class="caractCont">
+        <div class="carac" v-for="caracteristica in dialog.caracteristicas" :key="caracteristica.id" @click=quitar(caracteristica)>
+          <DinamicIcon :iconName="caracteristica?.urlicon" class="icono"/>
+          <p>{{ caracteristica.name }}</p>
+          <p class="cerrar">x</p>
+        </div>
+      </div>
+      <div class="btnCont2">
+        <div class="Btn2" @click="agregarCaracteristica(dialog.card)">ACEPTAR</div>
+        <div class="Btn2" @click="dialog.cancel">CANCELAR</div>
       </div>
     </div>
   </div>
@@ -72,6 +95,9 @@
 <script>
   import { Swiper, SwiperSlide } from 'swiper/vue'
   import 'swiper/css'
+  import DinamicIcon from '@/components/DinamicIcon.vue'
+  import util from '@/utils/utils'
+  import putMethod from '@/service/putMethod'
 
   export default {
     name:"DataDialog",
@@ -80,6 +106,7 @@
     components:{
       Swiper,
       SwiperSlide,
+      DinamicIcon,
     },
     emits: ["update-type", "update-rol"],
     computed: {
@@ -109,7 +136,27 @@
       },
       close(){
         this.$store.dispatch('setDialog', {})
-      }
+      },
+      agregar(car){
+        this.dialog.caracteristicas.push(car)
+        const idx = this.dialog.totalCarac.indexOf(car)
+        this.dialog.totalCarac.splice(idx,1)
+      },
+      quitar(car){
+        this.dialog.totalCarac.push(car)
+        const idx = this.dialog.caracteristicas.indexOf(car)
+        this.dialog.caracteristicas.splice(idx,1)
+      },
+      async agregarCaracteristica(card){
+        let dialog = {}
+        util.cargarLoader("Modificando características")
+        const characteristics = this.dialog.caracteristicas
+        card.characteristics = characteristics
+        const data = card
+        await putMethod.updateRoom(data)
+        util.cargarLoader("")
+        this.$store.dispatch('setDialog',dialog)
+      },
     },
   }
 </script>
@@ -246,12 +293,112 @@
   .Btn:hover {
     background-color: #99DCDD;
   }
+
+  .caracContenedor{
+    z-index: 2;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(0, 0, 0, 0.7);
+    .modificarCarac{
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      width: 80%;
+      height: auto;
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: #afedee;
+      color: var(--text);
+      border: 3px solid #0d7277;
+      border-radius: 10px;
+      label{
+        margin: 10px;
+      }
+      .caractCont{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        background-color: white;
+        border: 2px solid #0d7277;
+       border-radius: 10px;
+        padding: 15px;
+        .carac{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          border: 1px solid #0d7277;
+          border-radius: 10px;
+          background-color: #99DCDD;
+          width: auto;
+          height: auto;
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+          margin: 4px;
+          .icono{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: auto;
+            height: auto;
+            padding: 5px;
+          }
+          p{
+            font-size: 10px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 5px;
+            color: black;
+          }
+          .cerrar{
+            padding: 1px 5px ;
+            border: 1px solid #0d7277;
+            border-radius: 10px;
+          }
+        }
+        .carac:hover{
+          box-shadow: 0px 0px 5px 5px #99dcdd;
+        }
+      }
+      .btnCont2{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .Btn2{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 15px;
+          border: 2px solid #0f8389;
+          color: #0f8389;
+          cursor: pointer;
+          margin: 10px;
+          padding: 10px 50px;
+          font-weight: bold;
+          width: 50%;
+        }
+        .Btn2:hover {
+          background-color: #99DCDD;
+        }
+      }
+    }
+  }
+
   @media only screen and (max-width:480px ){
     #slider .swiper{
-        img {
-          width: 108%;
-          height: 30vw;
-        }
+      img {
+        width: 108%;
+        height: 30vw;
+      }
     }
     .contenedor{
       .filterCont{
@@ -269,7 +416,6 @@
       margin-top: -60%;
       }
     }
-
-}
+  }
 
 </style>
--- src/view/AdministrarCaracteristicas.vue
+++ src/view/AdministrarCaracteristicas.vue
@@ -2,14 +2,25 @@
   <div :class="[theme, 'card']">
     <div class="card-container">
       <div class="headerContainer">
-        <h2>CARACTERÍSTICAS:</h2>
         <div class="cerrar" @click="goBack">&lt;</div>
+        <h2>CARACTERÍSTICAS</h2>
+        <p @click="updateCar()" class="agregar">Agregar</p>
+      </div>
+      <div class="headerContainerDos">
+        <p>ID</p>
+        <p>Icon/Nombre</p>
+        <p>Actions</p>
       </div>
       <div class="footerCont">
         <div class="descripcion" v-for="car in card.characteristics" :key="car.id">
           <p>id: {{ car.id }}</p>
-          <DinamicIcon :iconName="car?.urlicon" />
-          <strong>{{ car?.name }}</strong>
+          <div class="iconNombre">
+            <DinamicIcon :iconName="car?.urlicon" />
+            <strong>{{ car?.name }}</strong>
+          </div>
+          <div class="action">
+            <p @click="deleteCar(car, card)">Eliminar</p>
+          </div>
         </div>
       </div>
     </div>
@@ -20,8 +31,9 @@
 <script>
 import util from '@/utils/utils'
 import getMethod from '@/service/getMethod'
+import putMethod from '@/service/putMethod'
 import DataDialog from '@/components/DataDialog.vue'
-import DinamicIcon from '@/components/DinamicIcon.vue';
+import DinamicIcon from '@/components/DinamicIcon.vue'
 
 export default {
   name: 'AdministrarCaracteristicas',
@@ -48,76 +60,159 @@ export default {
       const data = {
         id: this.$route.params.id,
         isId: true
-      };
+      }
       const resultado = await getMethod.getRoom(data)
-      console.log(resultado)
       this.card = resultado
-      console.log(this.card)
       util.cargarLoader("")
     },
     goBack() {
-      this.$router.go(-1);
+      this.$router.go(-1)
     },
+    async updateCar(){
+      util.cargarLoader("Cargando")
+      const carac = await getMethod.getCaracteristicas() || []
+      util.cargarLoader("")
+      let dialog = {
+        type: 'modificarCarac',
+        caracteristicas: this.card.characteristics,
+        totalCarac: carac,
+        card: this.card,
+        cancel: ()=> {
+          dialog = {}
+          this.$store.dispatch('setDialog',dialog)
+        }
+      }
+      this.$store.dispatch('setDialog',dialog)
+    },
+    async deleteCar(car, card){
+      let dialog = {
+        type: 'delete',
+        texto: 'Está seguro que desea eliminar esta característica?',
+        id: car.id,
+        name: car.name,
+        acept: async () =>{
+          dialog = {}
+          this.$store.dispatch('setDialog',dialog)
+          util.cargarLoader("Eliminando característica")
+          const characteristics = card.characteristics.filter(x => x.id != car.id)
+          card.characteristics = characteristics
+          const data = card
+          await putMethod.updateRoom(data)
+          util.cargarLoader("")
+        },
+        cancel: ()=> {
+          dialog = {}
+          this.$store.dispatch('setDialog',dialog)
+        }
+      }
+      this.$store.dispatch('setDialog',dialog)
+    }
   },
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .card{
     width: 100%;
-  }
-  .card-container{
-    width: 100%;
-    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     padding: 50px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  .headerContainer{
-    width: 85%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5px 20px;
-    color: #605B5B;
-  }
-  .cerrar{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #605B5B;
-    border: 3px #605B5B solid;
-    background-color: #cbcaca;
-    font-size: 30px;
-    font-weight: bold;
-    width: 45px;
-    height: 45px;
-    border-radius: 50%;
-    cursor: pointer;
-    margin-right: 40px;
-  }
-  p{
-    color: black;
-  }
-  .footerCont{
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    width: 80%;
-  }
-  .descripcion{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-  }
-  .descripcion p{
-    width: 85%;
-    padding-left: 5px;
-    text-align: left;
+    .card-container{
+      width: 90%;
+      height: auto;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .headerContainer{
+        width: 85%;
+        min-height: 50px;
+        height: auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 5px 20px;
+        color: #605B5B;
+        border: 1px #0d7277 solid;
+        background-color: #99dcdd;
+        border-radius: 15px;
+        .cerrar{
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color: #0f8389;
+          border: 2px #0d7277 solid;
+          background-color: #99dcdd;
+          font-size: 30px;
+          font-weight: bold;
+          width: 45px;
+          height: 45px;
+          border-radius: 50%;
+          cursor: pointer;
+        }
+        .agregar{
+          color: #0f8389;
+          border: 2px #0d7277 solid;
+          background-color: #99dcdd;
+          cursor: pointer;
+          border-radius: 15px;
+          padding: 5px 20px;
+          transition: .5s ease-in-out;
+        }
+        .agregar:hover{
+          color: #99dcdd;
+          border: 2px #074b4e solid;
+          background-color: #0d7277;
+        }
+      }
+      .headerContainerDos{
+        width: 85%;
+        min-height: 50px;
+        height: auto;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        padding: 5px 20px;
+        color: #605B5B;
+        border: 1px #0d7277 solid;
+        background-color: #99dcdd;
+        border-radius: 15px;
+        margin-top: 5px;
+      }
+      .footerCont{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        width: 85%;
+        .descripcion{
+          width: 100%;
+          border: 1px #0d7277 solid;
+          border-radius: 15px;
+          margin: 5px 0;
+          display: flex;
+          align-items: center;
+          justify-content: space-around;
+          p, strong{
+            color: black;
+            padding-left: 5px;
+            text-align: left;
+          }
+          .iconNombre {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .action{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+          }
+        }
+      }
+    }
   }
   @media only screen and (max-width:480px ){
     .headerContainer{
